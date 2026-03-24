015ba8e8ab79ae5c9f3e462496e2d9a97cc2ecfb
jugosoft <sardimka@gmail.com>
2026-03-24 23:31:09 +0400

    feat: deposit controller protection

--- src/modules/deposit/deposit.controller.ts
+++ src/modules/deposit/deposit.controller.ts
@@ -1,18 +1,21 @@
-import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, UseGuards } from '@nestjs/common';
 import { DepositInput } from './inputs/deposit.input';
 import { DepositService } from './services/deposit.service';
 import { Deposit } from 'src/entities/deposit.entity';
+import { AtGuard } from '@common/guards';
 
 @Controller('deposit')
 export class DepositController {
 
     public constructor(private readonly depositService: DepositService) { }
 
+    @UseGuards(AtGuard)
     @Get('list')
     public async getDepositList(): Promise<Deposit[]> {
         return await this.depositService.getDepositList();
     }
 
+    @UseGuards(AtGuard)
     @Get(':id')
     public async getDeposit(
         @Param('id') id: string
@@ -20,6 +23,7 @@ export class DepositController {
         return await this.depositService.getDeposit(id);
     }
 
+    @UseGuards(AtGuard)
     @Post('save')
     public async saveDeposit(
         @Body() deposit: DepositInput
